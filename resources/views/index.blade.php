127dfdfe7f13f3d80d2050beaf14d9b2727337f4
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-17 19:06:56 +0600

    Porfolio_img_added_toLaravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,7 +61,7 @@
                   </p>
               </div>
               <div class="col-md-6">
-                  <img src="image/myimage2.jpeg" alt="Your Image" class="img-fluid hero-img">
+                  <img src="{{asset('Frontend')}}/image/myimage2.jpeg" alt="Your Image" class="img-fluid hero-img">
               </div>
           </div>
       </div>
@@ -71,17 +71,17 @@
     <h1>Notable Projects and Achievements</h1>
   <div class="project-container">
     <div class="project">
-      <img src="image/todo.png" alt="Project 1">
+      <img src="{{asset('Frontend')}}/image/todo.png" alt="Project 1">
       <h3>Todo app</h3>
       <p>These apps have become popular tools for individuals, students, professionals, and teams to boost productivity, improve time management, and ensure tasks are completed efficiently. With a todo app, users can better stay on top of their responsibilities, set goals, and achieve their objectives more effectively.</p>
     </div>
     <div class="project">
-      <img src="image/slide-show.png" alt="Project 2">
+      <img src="{{asset('Frontend')}}/image/slide-show.png" alt="Project 2">
       <h3>Slide-show</h3>
       <p>Slideshow presentations are widely used in a range of settings, from business meetings and academic presentations to personal events like weddings or family gatherings. They are an effective way to showcase content, deliver information, and engage the audience visually.</p>
     </div>
     <div class="project">
-      <img src="image/guessing-game.png" alt="Project 3">
+      <img src="{{asset('Frontend')}}/image/guessing-game.png" alt="Project 3">
       <h3>Guessing-game</h3>
       <p>Guessing games can vary in complexity and can be adapted for different age groups and occasions. They are commonly used as icebreaker activities, entertainment at parties, or as educational tools to reinforce learning in a fun and interactive way.</p>
     </div>
