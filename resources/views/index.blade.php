e654b22aeae9120a0f1e399a815779e32b703908
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-17 19:29:19 +0600

    Footer_added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,31 +2,7 @@
     <div class="container">
   <header>
     <!-- navbar start -->
-    <nav class="navbar navbar-expand-md">
-      
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span><i class="fas fa-bars navbar-icon custom-icon"></i></span>
-      </button>
-      
-      <a class="navbar-brand" href="#" >
-        <img src="image/logo2.png" alt="">
-        <h5 class="logo-icon"><br>Md. Mahabubur Rahman</h5>
-      </a>
     
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="{{Route('homePage')}}">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{Route('aboutPage')}}">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{Route('contectPage')}}">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
     <!-- navbar end -->
     <section class="hero">
       <div class="container">
@@ -75,14 +51,4 @@
 
 
     
-
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"
-    ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+@include('include.footer')
